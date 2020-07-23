f9492d458aa34ea032d283a9bd06c3ec3f09b341
Courtney Zavaleta <courtneyzavaleta@gmail.com>
2020-07-23 15:08:49 +0100

    score and user answer completed

--- typescript-quiz/src/App.tsx
+++ typescript-quiz/src/App.tsx
@@ -36,7 +36,25 @@ const App = () => {
     setLoading(false);
   };
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      //User's answer
+      const answer = e.currentTarget.value;
+      // Check answer
+      const correct = questions[number].correct_answer === answer;
+      // Add score if correct
+      if (correct) setScore(prev => prev + 1);
+      // Save answer in array for user userAnswers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer
+      };
+
+      setUserAnswers(prev => [...prev, answerObject]);
+    }
+  };
 
   const nextQuestion = () => {};
 
@@ -61,10 +79,14 @@ const App = () => {
           callback={checkAnswer}
         />
       )}
-
-      <button className="next" onClick={nextQuestion}>
-        Next Question
-      </button>
+      {!gameOver &&
+      !loading &&
+      userAnswers.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <button className="next" onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
     </div>
   );
 };
--- typescript-quiz/src/components/QuestionCard.tsx
+++ typescript-quiz/src/components/QuestionCard.tsx
@@ -25,8 +25,8 @@ const QuestionCard: React.FC<Props> = ({
 
     <div>
       {answers.map(answer => (
-        <div>
-          <button disabled={userAnswer} onClick={callback}>
+        <div key={answer}>
+          <button disabled={userAnswer} onClick={callback} value={answer}>
             <span dangerouslySetInnerHTML={{ __html: answer }} />
           </button>
         </div>
